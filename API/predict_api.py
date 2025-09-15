056027d635ceb6434b2c757336747cc065af8748
Aman Gour <amangour3@gmail.com>
2025-09-15 06:35:35 +0530

    Track API folder as normal folder

---
+++ API/predict_api.py
@@ -0,0 +1,37 @@
+from fastapi import FastAPI
+from pydantic import BaseModel
+import joblib
+import pandas as pd
+from fastapi.middleware.cors import CORSMiddleware
+
+app = FastAPI()
+
+from fastapi.middleware.cors import CORSMiddleware
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # For testing: allow all origins
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+
+class ScoringItem(BaseModel):
+    meanTemp: float
+    maxTemp: float
+    minTemp: float
+    humidity: float
+    windSpeed: float
+    pressure: float
+    radiation: float
+
+
+# load your model (pipeline)
+model = joblib.load("model.pkl")
+
+@app.post("/predict")
+async def scoring_endpoint(item: ScoringItem):
+    df = pd.DataFrame([item.dict()])
+    yhat = model.predict(df)
+    return {"irrigation": float(yhat[0])}
